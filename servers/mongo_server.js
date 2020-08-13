71a7a4b70a380a48d1d78f1084f919865dcdf8cf
Dhiraj <dhiraj@bytemark.co>
2020-08-13 09:52:45 +0545

    node version added

--- servers/mongo_server.js
+++ servers/mongo_server.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
-connection_uri = 'mongodb://localhost:27017/MEANStackDB';
-// const connection_uri = 'mongodb+srv://dhiraj:dhiraj@cluster0.ocvoq.mongodb.net/test?retryWrites=true&w=majority';
+// connection_uri = 'mongodb://localhost:27017/MEANStackDB';
+const connection_uri = 'mongodb+srv://dhiraj:dhiraj@cluster0.ocvoq.mongodb.net/test?retryWrites=true&w=majority';
 
 exports.mongoConnection = function () {
 mongoose.connect(connection_uri, { useNewUrlParser: true, useCreateIndex: true }, (error) => {
